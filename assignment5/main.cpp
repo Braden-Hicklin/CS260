b49c9cdf9fdb0a3ec834eb3287d7fb2401ebea7d
D4ddyL0ngL3gs <52402651+D4ddyL0ngL3gs@users.noreply.github.com>
2023-02-15 15:09:03 -0800

    Update main.cpp

--- assignment5/main.cpp
+++ assignment5/main.cpp
@@ -6,36 +6,38 @@ using namespace std;
 
 int main()
 {
+    
     Auto_Sort sort;
     
     // main menu loop    
     int choice, new_value = 0;
-    while(choice != 6) {
-        cout << "========= Menu =========" << endl;
-        cout << "=      1. Add ID       =" << endl;
-        cout << "=      2. Mass Add ID  =" << endl;
-        cout << "=      3. Delete ID    =" << endl;
-        cout << "=      4. List IDs     =" << endl;
-        cout << "=      5. Search Pos   =" << endl;
-        cout << "=      6. Quit         =" << endl;
-        cout << "========================" << endl;
+    while(choice != 7) {
+        cout << "========== Menu ==========" << endl;
+        cout << "=       1. Add ID        =" << endl;
+        cout << "=       2. Mass Add ID   =" << endl;
+        cout << "=       3. Search For ID =" << endl;
+        cout << "=       4. List IDs      =" << endl;
+        cout << "=       5. Search Pos    =" << endl;
+        cout << "=       6. Delete ID     =" << endl;
+        cout << "=       7. Quit          =" << endl;
+        cout << "==========================" << endl;
         cin >> choice;
         
         switch(choice) {
             case(1):
                 cout << "Enter the ID you'd like to add: " << endl;
                 cin >> new_value;
-                sort.addID(new_value);
+                sort.add_id(new_value);
                 break;
             case(2):
                 cout << "How many ID's would you like to enter?: " << endl;
                 cin >> new_value;
-                sort.mass_addID(new_value);
+                sort.mass_add_id(new_value);
                 break;
             case(3):
-                cout << "Enter the ID you'd like to be removed: " << endl;
+                cout << "Enter the ID you're looking for: " << endl;
                 cin >> new_value;
-                sort.deleteID(new_value);
+                sort.pull_id(new_value);
                 break;
             case(4):
                 sort.display();
@@ -45,15 +47,21 @@ int main()
                 cout << 
                 "Enter the position of the ID you'd like to display: " << endl;
                 cin >> new_value;
-                sort.getID(new_value);
+                sort.get_pos(new_value);
+                break;
             case(6):
+                cout << "Enter the ID you'd like to be removed: " << endl;
+                cin >> new_value;
+                sort.delete_id(new_value);
+                break;
+            case(7):
                 break;
             default:
                 cout << "Invalid input, please try again" << endl;
                 break;
         }
-        
     }
-	return 0;
+    
+    return 0;
 }
 
